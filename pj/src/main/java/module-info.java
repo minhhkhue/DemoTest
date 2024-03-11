097b2f7ccb3c7d315c1b7a9a8cdf3688658b03d9
minhhkhue <khueminh0210@gmail.com>
2024-03-11 08:36:21 +0700

    FeatureTest

--- pj/src/main/java/module-info.java
+++ pj/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.pj {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
     opens com.mycompany.pj to javafx.fxml;
     exports com.mycompany.pj;
